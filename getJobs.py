6ad4d3bb301874d8e798c331739899f48fbf93be
taolakhue <taolakhue63@gmail.com>
2021-08-31 17:55:40 -0400

    draft

--- getJobs.py
+++ getJobs.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
@@ -12,17 +13,27 @@ driver.get("https://ca.indeed.com/jobs?q=computer%20science%20internship&l=Toron
 
 try:
     main = WebDriverWait(driver,10).until(
-        EC.presence_of_element_located((By.ID,"mosaic-zone-jobcards"))
+        EC.presence_of_element_located((By.ID,"resultsBodyContent"))
     )
 
     job_titles = main.find_elements_by_tag_name("h2")
-    b = main.find_elements_by_class_name("companyName")
-
+    company_name = main.find_elements_by_class_name("companyName")
+    """
     for i in range (len(job_titles)):
         print(job_titles[i].text)
-        print(b[i].text)
+        print(company_name[i].text)
+        job_titles[i].click()
+        #get content
+        time.sleep(1)
+    """
+    job_titles[0].click()
+    #Switch to another iFrame
+    a=main.find_element_by_id("vjs-container")
+    
+    #job_description = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.ID,"jobDescriptionText")))
+
     
 
 finally:
     time.sleep(5)
-    driver.quit()
+    
